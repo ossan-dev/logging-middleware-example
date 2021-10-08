7e8d7041c2afe395ce430a1967b875e1e1f771c5
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-10-08 10:26:24 +0200

    add helper methods for middleware

---
+++ logging-middleware-app/logging-middleware/Helpers/LogHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logging_middleware.Helpers
+{
+    public static class LogHelper
+    {
+        public static async Task EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+
+            string responseBodyPayload = await ReadResponseBodyAsync(httpContext.Response);
+
+            diagnosticContext.Set("Host", request.Host);
+            diagnosticContext.Set("RequestQueryString", request.QueryString);
+
+            diagnosticContext.Set("ResponseBody", responseBodyPayload);
+        }
+
+        private static async Task<string> ReadResponseBodyAsync(HttpResponse response)
+        {
+            response.Body.Seek(0, System.IO.SeekOrigin.Begin);
+            string responseBody = await new StreamReader(response.Body).ReadToEndAsync();
+            response.Body.Seek(0, System.IO.SeekOrigin.Begin);
+
+            return responseBody;
+        }
+    }
+}
---
+++ logging-middleware-app/logging-middleware/Helpers/StreamHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logging_middleware.Helpers
+{
+    public static class StreamHelper
+    {
+        public static string ReadStreamInChunks(Stream stream)
+        {
+            const int readChunkBufferLength = 4096;
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using var textWriter = new StringWriter();
+            using var reader = new StreamReader(stream);
+
+            var readChunk = new char[readChunkBufferLength];
+            int readChunkLength;
+
+            do
+            {
+                //  the position of the underlying stream is advanced of the number of chars read
+                // https://docs.microsoft.com/en-us/dotnet/api/system.io.streamreader.readblock?view=net-5.0 (at the bottom)
+                readChunkLength = reader.ReadBlock(readChunk, 0, readChunkBufferLength);
+                textWriter.Write(readChunk, 0, readChunkLength);
+            } while (readChunkLength > 0);
+
+            return textWriter.ToString();
+        }
+    }
+}
