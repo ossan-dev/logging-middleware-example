bffef64c7d4f413d5b8dc837e5c530d11489da61
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-10-08 08:58:17 +0200

    add model classes

---
+++ logging-middleware-app/logging-middleware/ApiModels/Input.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logging_middleware.ApiModels
+{
+    public class Input
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ logging-middleware-app/logging-middleware/ApiModels/Output.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logging_middleware.ApiModels
+{
+    public class Output
+    {
+        public string Message { get; set; }
+        public DateTime ProcessedAt { get; set; }
+    }
+}
